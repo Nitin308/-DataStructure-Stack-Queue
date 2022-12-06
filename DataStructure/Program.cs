360eee88c118cc6d49cb90e30bb44362a24a54d0
Nitin308 <nitin3081998@gmail.com>
2022-12-06 15:39:14 +0530

    Deleting Elements from a Queue

---
+++ DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+
+            LinkedList objList = new LinkedList();
+            objList.Enqueue(56);
+            objList.Enqueue(30);
+            objList.Enqueue(70);
+            objList.Display();
+            objList.Dequeue();
+            objList.Display();
+
+        }
+    }
+}
